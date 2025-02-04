8b1770cbe59dc1df1d360fb611f61368aa40b0e6
Ashsajal <ashsajal1@gmail.com>
2025-02-04 14:26:29 +0600

    feat: create home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
-import SideNav from "../components/partials/side-nav";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleOpen = () => {
-    setIsOpen(!isOpen)
-  }
+  const topics = [
+    "Physics 1st",
+    "Chemistry 1st",
+    "Biology 1st",
+    "Math 1st",
+    "ICT",
+  ];
   return (
     <div>
-      <SideNav handleClose={toggleOpen} isOpen={isOpen} />
-      <Button onClick={() => { setIsOpen(!isOpen) }}>Open dialog</Button>
+      <div className="grid grid-cols-2 gap-4">
+        {topics.map((topic) => (
+          <Card className="mb-2" key={topic}>
+            <CardContent>
+              <CardHeader>
+                <CardTitle>{topic}</CardTitle>
+              </CardHeader>
+
+              <CardFooter>
+                <Link to={`/quiz/${topic.split(" ").join("-")}`}><Button>Start</Button></Link>
+              </CardFooter>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
