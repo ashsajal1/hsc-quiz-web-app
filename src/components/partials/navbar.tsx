db5c89580852842e065bc9dc8f7e238c15645fc6
Ashsajal <ashsajal1@gmail.com>
2025-02-04 17:14:08 +0600

    feat: add exam nav

--- src/components/partials/navbar.tsx
+++ src/components/partials/navbar.tsx
@@ -4,14 +4,15 @@ import { CiMenuFries } from "react-icons/ci";
 import Text from "../custom-ui/text";
 import SideNav from "./side-nav";
 import { ModeToggle } from "../mode-toggle";
+import { Button } from "../ui/button";
 
 export default function Navbar() {
   const [scrollY, setScrollY] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -26,22 +27,34 @@ export default function Navbar() {
   }, []);
 
   const handleNavClick = () => {
-    if(isOpen) {
-      setIsOpen(false)
+    if (isOpen) {
+      setIsOpen(false);
     }
-  }
+  };
 
   return (
     <>
       <SideNav handleClose={toggleOpen} isOpen={isOpen} />
-      <nav className={`flex items-center justify-between p-2 w-full h-[80px] top-0 bg-white md:dark:border-none border-b dark:bg-black dark:border-b-gray-800 z-10 fixed ${scrollY > 150 ? 'bg-opacity-60 dark:bg-opacity-60 backdrop-blur' : ''}`} onClick={handleNavClick}>
-        <Link to='/'>
+      <nav
+        className={`flex items-center justify-between p-2 w-full h-[80px] top-0 bg-white md:dark:border-none border-b dark:bg-black dark:border-b-gray-800 z-10 fixed ${
+          scrollY > 150 ? "bg-opacity-60 dark:bg-opacity-60 backdrop-blur" : ""
+        }`}
+        onClick={handleNavClick}
+      >
+        <Link to="/">
           <Text label="HSC Quiz" className="text-xl font-bold" />
         </Link>
 
         <div className="flex items-center gap-2">
+          <Link to={"/exam"}>
+            <Button>Exam</Button>
+          </Link>
           <ModeToggle />
-          <CiMenuFries onClick={toggleOpen} className="h-6 w-6 md:hidden dark:text-white text-black" />
+
+          <CiMenuFries
+            onClick={toggleOpen}
+            className="h-6 w-6 md:hidden dark:text-white text-black"
+          />
 
           {/* <div className="hidden md:flex items-center gap-2">
             <Search />
@@ -50,5 +63,5 @@ export default function Navbar() {
         </div>
       </nav>
     </>
-  )
+  );
 }
