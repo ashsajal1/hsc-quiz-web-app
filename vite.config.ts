92c4ca3b1ae50e2ccac8476319395b5fb08e5170
Ashsajal <ashsajal1@gmail.com>
2025-02-04 23:34:22 +0600

    fix: increase build size

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import path from "path"
+import path from "path";
 
 const manifest = {
   name: "HSC Quiz",
@@ -96,35 +96,9 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: manifest,
-      // manifest: {
-      //   name: "React TSX Tailwind Template",
-      //   short_name: "ReactTTT PWA",
-      //   theme_color: "#ffffff",
-      //   icons: [
-      //     {
-      //       src: "pwa-64x64.png",
-      //       sizes: "64x64",
-      //       type: "image/png",
-      //     },
-      //     {
-      //       src: "pwa-192x192.png",
-      //       sizes: "192x192",
-      //       type: "image/png",
-      //     },
-      //     {
-      //       src: "pwa-512x512.png",
-      //       sizes: "512x512",
-      //       type: "image/png",
-      //       purpose: "any",
-      //     },
-      //     {
-      //       src: "maskable-icon-512x512.png",
-      //       sizes: "512x512",
-      //       type: "image/png",
-      //       purpose: "maskable",
-      //     },
-      //   ],
-      // },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4 * 1024 * 1024, // 4 MB
+      },
     }),
   ],
   resolve: {
@@ -133,5 +107,3 @@ export default defineConfig({
     },
   },
 });
-
-
