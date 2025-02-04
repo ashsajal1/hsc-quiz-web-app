8f735201501aa6ce87c6e6120b088d9e79ad76c2
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2025-02-04 14:11:25 +0600

    Initial commit

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/partials/navbar'
+import Footer from '../components/partials/footer'
+export default function RootLayout() {
+    return (
+        <>
+            <Navbar />
+            <main className='p-2 mt-[80px]'>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
