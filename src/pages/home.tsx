9b06b73fa4fef44f2a3d5ea461adc1d99603f96c
Ashsajal <ashsajal1@gmail.com>
2025-02-07 23:20:42 +0600

    feat: add label

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div>
       <McqCarousel />
+      <p className="my-4 font-bold">Select Topic</p>
       <div className="grid grid-cols-2 gap-4">
+        
         {topics.map((topic) => (
           <TopicCard key={topic.subject} topic={topic} />
         ))}
