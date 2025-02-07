d07c813b38fd6daa830938e20f2f57a37e8cd21a
Ashsajal <ashsajal1@gmail.com>
2025-02-07 22:20:18 +0600

    feat: add topics

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,24 +10,54 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   const topics = [
-    "Physics 1st",
-    "Chemistry 1st",
-    "Biology 1st",
-    "Math 1st",
-    "ICT",
+    {
+      name: "Physics 1st",
+      subject: "physics-1",
+    },
+    {
+      name: "Physics 2nd",
+      subject: "physics-2",
+    },
+    {
+      name: "Biology 1st",
+      subject: "biology-1",
+    },
+    {
+      name: "Biology 2nd",
+      subject: "biology-2",
+    },
+    {
+      name: "Chemistry 1st",
+      subject: "chemistry-1",
+    },
+    {
+      name: "Chemistry 2nd",
+      subject: "chemistry-2",
+    },
+    {
+      name: "ICT",
+      subject: "ict",
+    },
+    {
+      name: "Bangla",
+      subject: "bangla",
+    },
   ];
+
   return (
     <div>
       <div className="grid grid-cols-2 gap-4">
         {topics.map((topic) => (
-          <Card className="mb-2" key={topic}>
+          <Card className="mb-2" key={topic.subject}>
             <CardContent>
               <CardHeader>
-                <CardTitle>{topic}</CardTitle>
+                <CardTitle>{topic.name}</CardTitle>
               </CardHeader>
 
               <CardFooter>
-                <Link to={`/quiz/${topic.split(" ").join("-")}`}><Button>Start</Button></Link>
+                <Link to={`/quiz?subject=${topic.subject}`}>
+                  <Button>Start</Button>
+                </Link>
               </CardFooter>
             </CardContent>
           </Card>
