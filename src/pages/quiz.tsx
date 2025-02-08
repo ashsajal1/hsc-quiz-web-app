bbb0a97fae4143bc6fd12ce6a9e4285e8c525252
Ashsajal <ashsajal1@gmail.com>
2025-02-08 12:43:53 +0600

    fix: empty questiosn

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -3,14 +3,18 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function QuizPage() {
+  
   const handleQuizComplete = (score: number, total: number) => {
     alert(`Quiz completed! Score: ${score} out of ${total}`);
   };
 
   const location = useLocation();
+  const searchParam = new URLSearchParams(location.search);
+    const subjectParam = searchParam.get("subject");
+    const chapterParam = searchParam.get("chapter");
 
-  const [selectedTopic, setSelectedTopic] = useState<string>("1");
-  const [selectedChapter, setSelectedChapter] = useState<string>("0");
+  const [selectedTopic, setSelectedTopic] = useState<string>(subjectParam || "1");
+  const [selectedChapter, setSelectedChapter] = useState<string>(chapterParam ||"0");
 
   useEffect(() => {
     const searchParam = new URLSearchParams(location.search);
