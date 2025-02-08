078147a75e54acafb6644a7ffd48c196458abf22
Ashsajal <ashsajal1@gmail.com>
2025-02-08 12:32:37 +0600

    feat: add initial topic/chapter

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,13 +1,32 @@
 import { Quiz } from "@/components/quiz";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function QuizPage() {
   const handleQuizComplete = (score: number, total: number) => {
     alert(`Quiz completed! Score: ${score} out of ${total}`);
   };
 
+  const location = useLocation();
+
+  const [selectedTopic, setSelectedTopic] = useState<string>("1");
+  const [selectedChapter, setSelectedChapter] = useState<string>("0");
+
+  useEffect(() => {
+    const searchParam = new URLSearchParams(location.search);
+    const subjectParam = searchParam.get("subject");
+    const chapterParam = searchParam.get("chapter");
+    if (subjectParam) setSelectedTopic(subjectParam);
+    if (chapterParam) setSelectedChapter(chapterParam);
+  }, [location.search]);
+
   return (
     <div className="container mx-auto">
-      <Quiz onComplete={handleQuizComplete} />
+      <Quiz
+        onComplete={handleQuizComplete}
+        initialTopic={selectedTopic}
+        initialChapter={selectedChapter}
+      />
     </div>
   );
 }
