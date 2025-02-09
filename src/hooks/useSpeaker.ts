7d7faa3ce9329272c45ba2242fbac039215caa47
Ashsajal <ashsajal1@gmail.com>
2025-02-09 11:44:28 +0600

    feat: set lang bangla

--- src/hooks/useSpeaker.ts
+++ src/hooks/useSpeaker.ts
@@ -8,10 +8,16 @@ const useSpeaker = () => {
       console.warn("Speech Synthesis not supported in this browser.");
       return;
     }
-    
+    const voices = speechSynthesis.getVoices();
+    const bengaliVoice = voices.find((v) => v.lang === "bn-BD");
+    console.log(voices);
+
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.onstart = () => setIsSpeaking(true);
     utterance.onend = () => setIsSpeaking(false);
+
+    utterance.lang = "bn-IN";
+    utterance.voice = bengaliVoice || voices[0]; // Use default if no Bengali voice found
 
     window.speechSynthesis.speak(utterance);
     console.log("Speaking:", text);
