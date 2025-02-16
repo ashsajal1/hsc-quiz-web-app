08c85013546605c49511e0bcaa74f389f4de5168
Ashsajal <ashsajal1@gmail.com>
2025-02-16 12:32:10 +0600

    feat: speak mcq

--- src/components/mcq-card.tsx
+++ src/components/mcq-card.tsx
@@ -9,6 +9,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { Option } from "@/lib/type";
 import { InteractiveHoverButton } from "./magicui/interactive-hover-button";
+import { Volume2, VolumeX } from "lucide-react";
+import { useSpeakerStore } from "@/store/useSpeakerStore";
 
 interface QuestionCardProps {
   question: {
@@ -39,6 +41,7 @@ export function McqCard({
   onOptionClick,
   onNextQuestion,
 }: QuestionCardProps) {
+  const { speak, stop, isSpeaking } = useSpeakerStore();
   return (
     <Card
       className={`w-full mx-auto ${
@@ -60,20 +63,27 @@ export function McqCard({
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <p className="mb-4 font-semibold">
-          {question.description && (
-            <>
-              <p>{question.description}</p>
-              <br />
-            </>
+        <div className="flex gap-2 mb-4">
+          <p className="font-semibold">
+            {question.description && (
+              <>
+                <p>{question.description}</p>
+                <br />
+              </>
+            )}
+            {question.question.split("\n").map((line, index) => (
+              <React.Fragment key={index}>
+                {line}
+                <br />
+              </React.Fragment>
+            ))}
+          </p>
+          {isSpeaking ? (
+            <VolumeX onClick={stop} strokeWidth={1} />
+          ) : (
+            <Volume2 onClick={() => speak(question.question)} strokeWidth={1} />
           )}
-          {question.question.split("\n").map((line, index) => (
-            <React.Fragment key={index}>
-              {line}
-              <br />
-            </React.Fragment>
-          ))}
-        </p>
+        </div>
         <div className="gap-2 grid md:grid-cols-2">
           {question.options.map((option: Option) => (
             <Button
@@ -93,7 +103,9 @@ export function McqCard({
                     ? "bg-green-500 text-white"
                     : "bg-red-500 text-white"
                   : ""
-              } ${option.isCorrect && showAnswer ? "bg-green-500 text-white" : ""}`}
+              } ${
+                option.isCorrect && showAnswer ? "bg-green-500 text-white" : ""
+              }`}
             >
               {option.text}
             </Button>
