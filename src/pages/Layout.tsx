f3c107a224830dc953a523bf36991d081775a2fe
Ashsajal <ashsajal1@gmail.com>
2025-05-21 18:41:04 +0600

    feat: show when speaking

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/partials/navbar";
 import Footer from "../components/partials/footer";
-import { Volume2, VolumeX } from "lucide-react";
+import { VolumeX } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useSpeakerStore } from "@/store/useSpeakerStore";
 import { NuqsAdapter } from "nuqs/adapters/react";
@@ -15,13 +15,11 @@ export default function RootLayout() {
         <main className="flex-grow p-2 mt-[80px]">
           <Outlet />
         </main>
-        <Button size={"icon"} className="fixed bottom-6 right-6">
-          {isSpeaking ? (
+        {isSpeaking && (
+          <Button size={"icon"} className="fixed bottom-6 right-6">
             <VolumeX onClick={stop} strokeWidth={"1"} className="h-6 w-6" />
-          ) : (
-            <Volume2 onClick={stop} strokeWidth={"1"} className="h-6 w-6" />
-          )}
-        </Button>
+          </Button>
+        )}
         <Footer />
       </div>
     </NuqsAdapter>
