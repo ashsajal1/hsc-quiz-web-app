d09205ffa8014f6eca2ceadb3891a645f2e39d35
Ashsajal <ashsajal1@gmail.com>
2025-06-05 15:37:29 +0600

    feat: set select none

--- src/pages/drop-game.tsx
+++ src/pages/drop-game.tsx
@@ -342,7 +342,7 @@ export default function DropGame() {
   };
 
   return (
-    <div className="flex flex-col h-dvh w-screen overflow-hidden">
+    <div className="flex flex-col h-dvh w-screen overflow-hidden select-none">
       {/* Fixed Top Bar */}
       <header className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center">
         <div className="container mx-auto px-4 py-3 flex items-center justify-between">
